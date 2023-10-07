6efb6999ef5b37fd07fb896b4fd715591a896659
Adithya Gattadi <122796208+adithyagattadi@users.noreply.github.com>
2023-10-07 04:41:51 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+let score = "false"
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log(typeof(valueInNumber))
+
+// "33" = 33
+// "33abc" = NaN(not a number)
+// true=1, false=0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// isLoggedIn = true=1, false=0
+// "" = false
+// "Adi" = true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
