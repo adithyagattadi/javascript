3626034c2c4467f60fa875919f1c87769cf0a951
Adithya Gattadi <122796208+adithyagattadi@users.noreply.github.com>
2023-10-17 17:14:40 +0000

    objects 3

---
+++ 01_basics/10_objects3.js
@@ -0,0 +1,15 @@
+const course = {
+    coursename: "JS tutorials",
+    price: "999",
+    courseIn: "Adithya"
+}
+// console.log(course.coursename)
+
+const {coursename} = course
+console.log(coursename)
+
+{
+    "coursename"; "JS tutorials",
+    "price"; "999",
+    "courseIn"; "Adithya"
+}
