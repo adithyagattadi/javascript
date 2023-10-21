55d08aefe74f272dd887a52b73fcda9e3e37979b
Adithya Gattadi <122796208+adithyagattadi@users.noreply.github.com>
2023-10-21 16:31:23 +0000

    practice code

---
+++ 01_basics/practice.js
@@ -0,0 +1,73 @@
+// // primitive data type
+
+// const myName = "Adithya"                //string
+// const age = 123                         //number 
+// const isEligible = true                 //boolean
+// const outSide = null                    //object
+// const emailCheck = undefined            //undefined
+// const bigNumber = 293882983881n         //bigint
+// const mySym = Symbol("hello")           //symbol
+
+
+// // non primitive data type
+
+// const heros = ["ok", "an", "is", "it"]  //object
+
+// const myObj = {
+//     name: "Adi",                        //object
+//     age: 20,
+//     isEligible: true
+// }
+
+// const myFunc = function(){
+//     console.log("Hello")                //function
+// }
+
+
+// // outputs
+
+// console.log(typeof(myName))
+// console.log(typeof(age))
+// console.log(typeof(isEligible))
+// console.log(typeof(outSide))
+// console.log(typeof(emailCheck))
+// console.log(typeof(bigNumber))
+// console.log(typeof(mySym))
+
+// console.log(typeof(heros))
+// console.log(typeof(myObj))
+// console.log(typeof(myFunc))
+
+// ************************************ nums and maths ***************************************
+
+// const myNum = 100.46
+// console.log(myNum.toFixed(1))
+// console.log(myNum.toString())
+// console.log(myNum.toLocaleString())
+// console.log(myNum.toPrecision(3))
+
+
+// console.log(Math.abs(-10))
+// console.log(Math.round(6.2))
+// console.log(Math.ceil(5.8))
+// console.log(Math.sqrt(36))
+// console.log(Math.min(23,54,66,2,6,88,50))
+// console.log(Math.min(23,54,66,2,6,88,50))
+// console.log(Math.random())
+// console.log(Math.random()*10+1)
+
+// const min = 10
+// const max = 30
+// console.log(Math.floor(Math.random() * max-min+1 + min))
+// console.log(Math.floor(Math.random() * max-min+1 + max))
+
+// const numerrr = new Number(100000000)
+// console.log(numerrr.toLocaleString('en-IN'))
+
+// ************************************ Dates ***************************************
+
+// const myDate = new Date()
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toString())
+// console.log(myDate.getMinutes())
+// console.log(myDate.getMonth()+1)
